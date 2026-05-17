e2e031c1718bdef82a3e3534d21d8253e97865d4
SanmuganathanLathusan <sanmuganathanlathusan@gmail.com>
2026-05-17 11:12:01 +0530

    modified  backend/config/db.js file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  if (!process.env.MONGO_URI) {
+    console.error("CRITICAL ERROR: MONGO_URI is not defined in environment variables.");
+    return;
+  }
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
-    process.exit(1);
   }
 };
 
